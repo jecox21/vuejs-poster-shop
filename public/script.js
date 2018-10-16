0873415f515e930dfb4cb220d31d9f412dd22a72
jecox21 <jecox21@gmail.com>
2018-10-16 10:12:06 -0400

    updates after completing project

--- public/script.js
+++ public/script.js
@@ -1,1 +1,98 @@
-console.log('It works.');
+var PRICE = 9.99;
+var LOAD_NUM = 10;
+
+new Vue({
+    el: '#app',
+    data: {
+        total: 0,
+        items: [],
+        cart: [],
+        results: [],
+        newSearch: 'anime',
+        lastSearch: '',
+        loading: false,
+        price: PRICE
+    },
+    methods: {
+        appendItems: function() {
+            if (this.items.length < this.results.length) {
+                var append = this.results.slice(this.items.length, this.items.length + LOAD_NUM);
+                this.items = this.items.concat(append);
+                console.log('results: '.concat(this.results.length));
+                console.log('items: '.concat(this.items.length));
+            }
+        },
+        onSubmit: function() {
+            if (this.newSearch.length) {
+                this.loading = true;
+                this.items = [];
+                this.results = [];
+                this.$http
+                    .get('/search/'.concat(this.newSearch))
+                    .then(function(res) {
+                        this.lastSearch = this.newSearch;
+                        this.results = res.data;
+                        this.appendItems();
+                        this.loading = false;
+                    });
+            }
+
+        },
+        addItem: function(index) {
+            this.total += PRICE;
+            var item = this.items[index];
+            var found = false;
+            for (var i = 0; i < this.cart.length; i++) {
+                if (this.cart[i].id === item.id) {
+                    this.cart[i].qty++;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                this.cart.push({
+                    id: item.id,
+                    title: item.title,
+                    qty: 1,
+                    price: PRICE
+                });
+            }
+        },
+        inc: function(item) {
+            item.qty++;
+            this.total += PRICE;
+        },
+        dec: function(item) {
+            item.qty--;
+            if (item.qty <= 0) {
+                for (var i = 0; i < this.cart.length; i++) {
+                    if (this.cart[i].id === item.id) {
+                        this.cart.splice(i, 1);
+                        break;
+                    }
+                }
+            }
+            this.total -= PRICE;
+        }
+    },
+    filters: {
+        currency: function(price) {
+            return '$'.concat(price.toFixed(2));
+        }
+    },
+    computed: {
+        noMoreItems: function() {
+            return this.items.length === this.results.length && this.results.length > 0
+        }
+    },
+    mounted: function() {
+        this.onSubmit();
+
+        var vueInstance = this;
+        var elem = document.getElementById('product-list-bottom')
+        var watcher = scrollMonitor.create(elem);
+        watcher.enterViewport(function() {
+            vueInstance.appendItems();
+        })
+    }
+});
